db7a70099c929a4ea5a3e690920b0150e5f47a19
NazarHaida <nazar.haida.knm.2020@lpnu.ua>
2025-02-18 21:07:44 +0200

    Phone Catalog

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Phone-Catalog/',
+  base: './', // <--- [NEW] add this line
   resolve: {
     alias: {
       'react-redux': require.resolve('react-redux'),
